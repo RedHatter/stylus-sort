3187a7ecfeb83cbf492ec8ee22e989294034d6e7
Timothy Johnson <timothy@idioticdev.com>
2016-07-15 14:55:59 -0700

    Fix '--config' cli option.

--- bin/stylus-sort.js
+++ bin/stylus-sort.js
@@ -4,8 +4,8 @@ const Sort = require('../lib')
 const cli = require('commander')
 
 cli
-  .version('1.0.0')
-  .option('-c --config', 'configuration to sort by')
+  .version('1.0.1')
+  .option('-c, --config [file]', 'configuration to sort by')
   .arguments('<path...>')
   .action(path => {
     let sort = new Sort(cli.config || 'zen')
